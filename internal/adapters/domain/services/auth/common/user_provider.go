202427142ac6bfeccc962a7ad476525b5e53585c
nomadhills <75098873+nomadhills@users.noreply.github.com>
2023-10-15 12:56:28 -0400

    simple project structure

--- internal/adapters/domain/services/auth/common/user_provider.go
+++
@@ -1,8 +0,0 @@
-package common
-
-type UserProvider interface {
-	FindByEmail(email string) (User, error)
-	FindByID(id string) (User, error)
-	FindByUsername(username string) (User, error)
-	NewBasic(email string, password string) (User, error)
-}
